b270aa260c9bfd7cd244cfd7cca46533885f8bb3
oishbdh <ikechukwuarinze614@gmail.com>
2022-09-30 00:10:48 +0100

    Web_api

---
+++ TestCoreApi/TestCoreApi/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestCoreApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet(Name = "Getint")]
+        public int Get(int id)
+        {
+            return id;
+        }
+    }
+}
---
+++ TestCoreApi/TestCoreApi/Controllers/HomeController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestCoreApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController1 : Controller
+    {
+        [HttpGet(Name = "Getstring")]
+        public string Get()
+        {
+            return "I love c#";
+        }
+    }
+}
